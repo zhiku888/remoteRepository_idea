081275dcf3f121a5a8d87618a94712fe1e49fcfe
zhikudaojie <ly@51dcool.com>
2020-06-07 12:52:22 +0800

    提交

---
+++ git_day01_demo/src/main/java/com/daojie/pojo/User.java
@@ -0,0 +1,13 @@
+package com.daojie.pojo;
+
+/**
+ * @ClassName User
+ * @Description TODO
+ * @Author ly
+ * @Date 2020/6/7 12:51
+ * @Version V1.0
+ */
+public class User {
+    private Integer id;
+    private String name;
+}
